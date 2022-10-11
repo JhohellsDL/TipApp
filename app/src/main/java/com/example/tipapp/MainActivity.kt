740b70ada4a94e565818a79b8a84004f5b6c9e27
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2022-10-11 14:32:58 -0500

    funcionalidad add

--- app/src/main/java/com/example/tipapp/MainActivity.kt
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.tipapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tipapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalcular.setOnClickListener { calcularPropina() }
+    }
+
+    private fun calcularPropina() {
+        val stringInTextField = binding.costoDeServicio.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+
+        if (cost == null){
+            binding.montoPropina.text = ""
+            return
+        }
+
+        val porcentajePropinas = when (binding.opcionesPropina.checkedRadioButtonId) {
+            R.id.opcion_veinte_porciento -> 0.20
+            R.id.opcion_diesiocho_porciento -> 0.18
+            else -> 0.15
+        }
+
+        var propina = cost * porcentajePropinas
+        if(binding.rendondeoSwitch.isChecked){
+            propina = ceil(propina)
+        }
+
+        val propinaFormateada = NumberFormat.getCurrencyInstance().format(propina)
+        binding.montoPropina.text = getString(R.string.monto_de_propina, propinaFormateada)
     }
 }
